0cfaeb400a404a9cc1eb60b1e0c5596bd9516c7c
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-07-08 23:12:19 -0300

    feat: professional card reads and rendes user's professional contacts

--- src/components/professional-card/professional-card-component.tsx
+++ src/components/professional-card/professional-card-component.tsx
@@ -7,31 +7,33 @@ import {
 import { DividerComponent } from "../divider/divider-component";
 import { CallButton } from "../call-button/call-button-component";
 import { CallButtonColor } from "../call-button/call-button-component";
+import { useAppSelector } from "../../redux/hooks";
+import { currentUserSelector } from "../../redux/user/user-slice";
 
 export const ProfessionalCardComponent = () => {
+  const { professional_card } = useAppSelector(currentUserSelector);
+
   return (
     <MainCardComponent>
       <ProfessionalCard>
         <h3>Contatos Profissionais</h3>
         <ProfessionalContactList>
-          <ProfessionalContact>
-            <p>Dr. Higor Cristian</p>
-            <DividerComponent color="var(--text-white)" />
-            <p>Médico responsável</p>
-            <CallButton color={CallButtonColor.purple}>Ligar</CallButton>
-          </ProfessionalContact>
-          <ProfessionalContact>
-            <p>Jorge Henrique</p>
-            <DividerComponent color="var(--main-card-bg)" />
-            <p>Cuidador</p>
-            <CallButton color={CallButtonColor.purple}>Ligar</CallButton>
-          </ProfessionalContact>
-          <ProfessionalContact>
-            <p>Maria Alice</p>
-            <DividerComponent color="var(--main-card-bg)" />
-            <p>Cuidadora</p>
-            <CallButton color={CallButtonColor.purple}>Ligar</CallButton>
-          </ProfessionalContact>
+          {professional_card && professional_card.length ? (
+            professional_card.slice(0, 4).map((item) => {
+              return (
+                <ProfessionalContact>
+                  <p>{item.name}</p>
+                  <DividerComponent color="var(--text-white)" />
+                  <p>{item.job}</p>
+                  <CallButton color={CallButtonColor.purple} href={`tel:${item.phone_number}`}>
+                    Ligar
+                  </CallButton>
+                </ProfessionalContact>
+              );
+            })
+          ) : (
+            <p>Você não possui contato profissional</p>
+          )}
         </ProfessionalContactList>
       </ProfessionalCard>
     </MainCardComponent>
