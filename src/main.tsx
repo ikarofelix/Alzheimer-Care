201606dce4f40d7ecd515716612a9b27cce3a41d
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-08-02 10:14:40 -0300

    fix: client and server communication

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { store } from "./redux/store";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: "https://alzheimer-care-server.vercel.app/graphql",
+  uri: "https://alzheimer-care-server.vercel.app/",
   credentials: "include",
 });
 
